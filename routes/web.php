5e0e5fb9a5dc3f644c5e34410e5338657273743e
Jakub Polák <polakjakub@gmail.com>
2021-04-01 18:49:10 +0200

    user auth

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('company', \App\Http\Controllers\CompanyController::class);
+
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
